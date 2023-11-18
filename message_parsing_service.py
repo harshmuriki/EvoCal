10c2d12787423219de4786f0ff7239bb7989ba13
Chinar Dankhara <chinardankhara@gmail.com>
2023-11-18 16:22:23 -0500

    flask api created

--- message_parsing_service.py
+++ message_parsing_service.py
@@ -1,35 +1,32 @@
 from openai import OpenAI
 import re
+import pickle
+from flask import Flask, request, jsonify
+import json
 
-def send_message(message):
+def classify_message(message):
     few_shot_prompt = f"""
         Instruction: Read the email content and classify it as '1' if it contains information about a specific event (like a meeting, party, or gathering) with a particular place AND time and '0' if it does not contain any specific event information.
 
         Email: "Hi everyone, Just a quick reminder about our study group session for the upcoming exams. We're meeting this Wednesday at 6 PM in the library's main study hall. Bring your notes, and let's ace these tests together! See you there, Alex"
         Classification: Yes
 
-        Email: "Dear Colleagues, Please be reminded of our faculty meeting focused on the new curriculum development. The meeting is scheduled for Friday, 10 AM, in the main conference room (Building A). We'll discuss the proposed changes and gather feedback. Regards, Prof. Elizabeth Johnson"
-        Classification: Yes
-
         Email: "Hello fellow students, As exams are approaching, I wanted to share some helpful study resources with you all. I've compiled a list of useful textbooks, online articles, and practice quizzes. Let's make the most of these materials to prepare well! Best, Jordan"
         Classification: No
 
-        Email: "Team, I've attached the summary of our team's performance for this month. Great work on achieving our targets! Let's keep this momentum going and brainstorm on how we can further improve in the coming month. I look forward to our continued success. Best, Linda"
-        Classification: No
-
         Email: "{message}"
-        Classification: [RETURN ONLY THE DIGIT HERE]
+        Classification:
         """
     client = OpenAI(api_key = "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")
 
     completion = client.chat.completions.create(
-    model="gpt-4-0613",
+    model="gpt-4-1106-preview",
     messages=[
         {"role": "system", "content": "You are a helpful assistant."},
         {"role": "user", "content": f"{few_shot_prompt}"}
     ]
     )
-    return completion
+    return True if "1" in completion.choices[0].message.content else False
 
 def process_messages(file_name):
     #TODO: method might need to change as we move away from local txt files
@@ -38,23 +35,71 @@ def process_messages(file_name):
     #remove empty or whitespace only strings
     emails = [i for i in emails if i]
     #links = [re.findall(r'(https?://\S+)', i) for i in emails]    
-    emails = [re.sub(r'\n\s*\n', '\n', i) for i in emails]
+    
     emails = [re.sub(r'https?://\S+', '', i) for i in emails]
     # Regular Expression to remove empty or incomplete brackets, parentheses, etc.
     pattern = r'\[\]|\(\)|\{\}|[\[\({<](?=[\n\r]|$)'
-
+    emails = [re.sub(r'\n\s*\n', '', i) for i in emails]
     #pattern = r'\[\s*\]|\(\s*\)|\{\s*\}|[\[\(\{]\s*($|\n)'
     emails = [re.sub(pattern, '', email) for email in emails]
     return emails
-    pass
-    
 
+def extract_events(email):
+    extraction_prompt = f"""
+    Instruction: Analyze the provided email and extract event information into a list of structured JSON objects. Each JSON object should contain the tags: "name" for the event name, "time" for the time of the event, and "location" for the location of the event. Be aware that:
+
+        - There may be multiple events mentioned in a single email. Each event should be represented as a separate JSON object in the list.
+        - Real-world email data can be messy, with random line breaks, irrelevant words, unicode characters, and addresses that may not be related to an event. Ensure robustness in handling such irregularities.
+        - Even if there's only one event, the information should still be formatted as a list containing a single JSON object.
+        - Your message will be fed to an automatic parser. Return the JSON list and nothing else. This is mission critical.
+
+        Email: {email}
+        Extracted Information:
+    """
+    client = OpenAI(api_key = "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")
+
+    completion = client.chat.completions.create(
+    model = "gpt-3.5-turbo-1106",
+    response_format = { "type": "json_object"},
+    messages=[
+        {"role": "system", "content": "You are a helpful assistant."},
+        {"role": "user", "content": f"{extraction_prompt}"}
+    ]
+    )
+    return completion.choices[0].message.content
+
+def master_service(file_name):
+    emails = process_messages(file_name)
+    #keep only those that return true for classify_message
+    emails = [i for i in emails if classify_message(i)]
+    if len(emails) == 0:
+        return []
+    events = [extract_events(i) for i in emails]
+    events = [json.loads(i)["events"] for i in events][0]
+    return events
+
+app = Flask(__name__)
+@app.route('/process_file', methods=['POST'])
+def process_file():
+    try:
+        file_name = request.json['file_name']
+        result = master_service(file_name)
+        return jsonify(result)
+    except Exception as e:
+        return jsonify({'error': str(e)}), 500
 if __name__ == '__main__':
-    # message = "Dear All,I'm excited to announce that our team has achieved a major milestone in the development of our new software. This success is a testament to the hard work and dedication of each team member.In light of this achievement, I would like to acknowledge everyone's effort and encourage you to keep up the excellent work. Our focus now shifts to the next phase of development, where your continued contributions will be crucial.Remember to update your task statuses in our project tracking tool, and let's maintain our momentum going forward."
-    # response = send_message(message)
-    # print(response.usage.prompt_tokens)
-    # print(response.usage.completion_tokens)
-    # print(response.choices[0].message)
-    message = process_messages("email_chinarshital.txt")
-    print(len(message))
-    print(message[0])
+    app.run(debug=True)
+    pass
+
+
+# if __name__ == '__main__':
+#     # message = "Dear All,I'm excited to announce that our team has achieved a major milestone in the development of our new software. This success is a testament to the hard work and dedication of each team member.In light of this achievement, I would like to acknowledge everyone's effort and encourage you to keep up the excellent work. Our focus now shifts to the next phase of development, where your continued contributions will be crucial.Remember to update your task statuses in our project tracking tool, and let's maintain our momentum going forward."
+#     # response = classify_message(message)
+#     # print(response)
+#     # message = process_messages("email_chinarshital.txt")
+#     # print(len(message))
+#     # print(extract_events(message[4]))
+#     pass
+#     with open("output.pkl", "wb") as f:
+#         pickle.dump(master_service("email_chinarshital.txt"), f)
+    
