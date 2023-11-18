975a46698d0bb849ec2caaafc4b0ad6c1dfb556d
Chinar Dankhara <chinardankhara@gmail.com>
2023-11-18 01:06:12 -0500

    gmail service
    gmail service works. no flask api, just plain python

--- gmail_fetch_service.py
+++ gmail_fetch_service.py
@@ -1,55 +1,60 @@
-import os.path
-
-from google.auth.transport.requests import Request
-from google.oauth2.credentials import Credentials
 from google_auth_oauthlib.flow import InstalledAppFlow
 from googleapiclient.discovery import build
-from googleapiclient.errors import HttpError
+import os
+from google.oauth2.credentials import Credentials
+from google.auth.transport.requests import Request
+import base64
 
-# If modifying these scopes, delete the file token.json.
-SCOPES = ["https://www.googleapis.com/auth/gmail.readonly"]
+def get_gmail_service():
+    SCOPES = ['https://www.googleapis.com/auth/gmail.readonly']
+    creds = None
 
+    if os.path.exists(f'token.json'):
+        creds = Credentials.from_authorized_user_file(f'token.json', SCOPES)
+    #TODO: Add a check to see if the token is expired
+    #TODO: User needs an option to select a different email account if needed 
+    if not creds or not creds.valid:
+        if creds and creds.expired and creds.refresh_token:
+            creds.refresh(Request())
+        else:
+            flow = InstalledAppFlow.from_client_secrets_file('credentials.json', SCOPES)
+            creds = flow.run_local_server(port=8080)
 
-def main():
-  """Shows basic usage of the Gmail API.
-  Lists the user's Gmail labels.
-  """
-  creds = None
-  # The file token.json stores the user's access and refresh tokens, and is
-  # created automatically when the authorization flow completes for the first
-  # time.
-  if os.path.exists("token.json"):
-    creds = Credentials.from_authorized_user_file("token.json", SCOPES)
-  # If there are no (valid) credentials available, let the user log in.
-  if not creds or not creds.valid:
-    if creds and creds.expired and creds.refresh_token:
-      creds.refresh(Request())
-    else:
-      flow = InstalledAppFlow.from_client_secrets_file(
-          "credentials.json", SCOPES
-      )
-      creds = flow.run_local_server(port=8080)
-    # Save the credentials for the next run
-    with open("token.json", "w") as token:
-      token.write(creds.to_json())
+        with open(f'token.json', 'w') as token:
+            token.write(creds.to_json())
 
-  try:
-    # Call the Gmail API
-    service = build("gmail", "v1", credentials=creds)
-    results = service.users().labels().list(userId="me").execute()
-    labels = results.get("labels", [])
+    return build('gmail', 'v1', credentials=creds)
 
-    if not labels:
-      print("No labels found.")
-      return
-    print("Labels:")
-    for label in labels:
-      print(label["name"])
+def get_unread_emails(service):
+    user_info = service.users().getProfile(userId='me').execute()
+    user_email = user_info['emailAddress']
+    results = service.users().messages().list(userId='me', labelIds=['INBOX'], q='is:unread').execute()
+    messages = results.get('messages', [])
+    f = open(f'email_{user_email.split("@")[0]}.txt', 'w')
 
-  except HttpError as error:
-    # TODO(developer) - Handle errors from gmail API.
-    print(f"An error occurred: {error}")
+    for message in messages:
+        msg = service.users().messages().get(userId='me', id=message['id'], format='full').execute()
+        payload = msg['payload']
+        headers = payload.get('headers')
 
+        subject = next(header['value'] for header in headers if header['name'] == 'Subject')
+        f.write(f'Subject: {subject}\n')
+    
+        if 'parts' in payload:
+            for part in payload['parts']:
+                if part['mimeType'] == 'text/plain' or part['mimeType'] == 'text/html':
+                    data = part['body']['data']
+                    decoded_data = base64.urlsafe_b64decode(data.encode('ASCII'))
+                    f.write(decoded_data.decode('utf-8'))
+                    break
+        else:
+            data = payload['body']['data']
+            decoded_data = base64.urlsafe_b64decode(data.encode('ASCII'))
+            f.write(decoded_data.decode('utf-8'))
+        
+        f.write("!@#$%^&*()" + "\n\n")
+    f.close()
 
-if __name__ == "__main__":
-  main()
+if __name__ == '__main__':
+    service = get_gmail_service()
+    get_unread_emails(service)
