2f915a062e5f09d011c4bd281c44c9a7d32ebbf5
harshmuriki <harshsuhith@gmail.com>
2023-11-19 04:46:37 -0500

    added proper frontend

--- pages/index.js
+++ pages/index.js
@@ -5,58 +5,87 @@ import React, { useState, useEffect } from 'react';
 const Home = () => {
     const [currentGoogleAccessToken, setGoogleAccessToken] = useState('');
     const { data: GoogleSession } = useSession();
-    console.log("AllDataFromGoogle:", GoogleSession);
-    console.log("GoogleAccessToken:", GoogleSession?.user?.accessToken);
+    const [isDarkMode, setIsDarkMode] = useState(false);
+
+    // console.log("AllDataFromGoogle:", GoogleSession);
+    // console.log("GoogleAccessToken:", GoogleSession?.user?.accessToken);
+    const toggleDarkMode = () => {
+        setIsDarkMode(prevMode => !prevMode);
+    };
     useEffect(() => {
         if (GoogleSession) {
             // Set the Google access token when the GoogleSession object is available
             setGoogleAccessToken(GoogleSession.user.accessToken);
-            
+
             if (currentGoogleAccessToken) {
                 // Send the currentGoogleAccessToken to an endpoint
                 fetch('https://uploadtoken-3l6j2umkza-uc.a.run.app', {
+                    mode: 'no-cors',
                     method: 'POST',
                     headers: {
                         'Content-Type': 'application/json'
                     },
                     body: JSON.stringify({ accessToken: currentGoogleAccessToken })
                 })
-                .then(response => {
-                    // Handle the response
-                })
-                .catch(error => {
-                    // Handle the error
-                });
+                    .then(response => {
+                        // Handle the response
+                    })
+                    .catch(error => {
+                        // Handle the error
+                    });
             }
         }
     }, [GoogleSession, currentGoogleAccessToken]);
 
     return (
-        <div className={styles.container}>
-            {!GoogleSession ? (
-                <div className={styles.notSignedIn}>
-                    <h1>Welcome To EvoCal</h1>
-                    <p>Please sign in to start the automated AI processes</p>
-                    <button className={styles.signInButton} onClick={() => signIn()}>
-                        Sign in
-                    </button>
-                </div>
-            ) : (
-                <main className={styles.main}>
-                    <div className={styles.header}>
-                        <h4>Signed in as {GoogleSession.user.name}</h4>
-                        {/* show the profile image */}
-                        <img src={GoogleSession.user.picture} className={styles.profileImage} />
-                        <button className={styles.signOutButton} onClick={() => signOut()}>
-                            Sign out
-                        </button>
+        <div className={`min-h-screen ${isDarkMode ? 'bg-primary text-primary' : 'bg-white text-black'}`}>
+            <div class="flex items-center justify-center h-screen">
+                <div class="w-1/2 h-1/2">
+                    <div className="container md bg-gray-300 text-black py-10 px-5 text-4xl rounded-3xl">
+                        <p className="mt-10">Welcome To EvoCal!!</p>
                     </div>
-                    {/* Additional content when signed in */}
-                    <p>You are signed into the system</p> {/* Added text */}
-
-                </main>
-            )}
-        </div>
+                    <div className="container md bg-gray-200 text-black p-5 text-xl rounded-3xl mt-1 mb-10">
+                        A tool automatically add events from Gmail to Google Calendar
+                    </div>
+                    <div className="container lg flex items-center justify-center">
+                        {!GoogleSession ? (
+                            <div className="flex flex-col items-center">
+                                <div className="container md bg-gray-300 text-black p-5 text-xl rounded-3xl ${isDarkMode ? 'bg-primary text-primary' : 'bg-white text-black'}">
+                                    Please sign in to start the automated AI processes
+                                </div>
+                                <button className={`inline-block mt-4 bg-green-400 text-white p-4 text-xl flex items-center rounded-3xl`} onClick={() => signIn()}>
+                                    Sign in
+                                </button>
+                            </div>
+                        ) : (
+                            <main className={styles.main}>
+                                <div className="container md bg-gray-200 text-black p-2 text-mg rounded-3xl mb-5">
+                                    Your Email is subscribed to our service
+                                </div>
+                                <div className="flex items-center">
+                                    <img src={GoogleSession.user.picture} className="rounded-3xl w-auto h-auto mr-4 mb-4" />
+                                    <div className="flex flex-col flex items-center">
+                                        <h4 className="text-center w-full">
+                                            <span style={{ fontWeight: "bold" }}>
+                                                Welcome,<br />
+                                                {GoogleSession.user.name}{" "}
+                                                <span style={{ fontSize: "1.5em" }}>{String.fromCharCode(0x263A)}</span>:
+                                            </span>
+                                        </h4>
+                                        <button className={`inline-block w-auto md p-1 bg-gray-300 rounded-3xl mb-4 ${styles.signOutButton}`} onClick={() => signOut()}>
+                                            Sign out
+                                        </button>
+                                    </div>
+                                </div>
+                                {/* <button className={`inline-block bg-gray-400 rounded-3xl py-2 px-4 `} onClick={toggleDarkMode}>
+                                    Toggle Theme
+                                </button> */}
+                            </main>
+                        )}
+                    </div>
+                </div>
+            </div >
+        </div >
     );
 };
 
--- tailwind.config.js
+++ tailwind.config.js
@@ -7,10 +7,14 @@ module.exports = {
   ],
   theme: {
     extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+      colors: {
+        black: '#000', // Set black color
+      },
+      backgroundColor: {
+        primary: 'black', // Set black as the primary background color
+      },
+      textColor: {
+        primary: 'white', // Set white as the primary text color
       },
     },
   },
